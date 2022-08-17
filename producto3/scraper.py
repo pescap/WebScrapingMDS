1a4093baf3526285f5518a8a6720c76feb3b74de
Paul Escapil-Inchauspé <paul@MacBook-Air-de-Paul-2.local>
2022-08-17 03:27:04 +0200

    scraper is fine

--- producto3/scraper.py
+++ producto3/scraper.py
@@ -4,47 +4,61 @@ import re
 import requests
 
 # número de issue
+
+run = True
 issue = 1
-
-# url del issue
-url_name = "https://github.com/pescap/WebScrapingMDS/issues/" + str(issue)
-
-# definición del DataFrame
 columns = ["issue", "author", "state", "assignees"]
 df = pd.DataFrame(columns=columns)
 
-# Recuperar el html
-url = requests.get(url_name)
-soup = BeautifulSoup(url.text, "html.parser")
+while run:
+    url_name = "https://github.com/pescap/WebScrapingMDS/issues/" + str(issue)
 
-# Imprimir el código html de la página
-# print(soup.prettify())
+    # Recuperar el html
+    url = requests.get(url_name)
+    soup = BeautifulSoup(url.text, "html.parser")
 
-state = "open"
+    if str(soup) == "Not Found":
+        run = False
 
-# if encuentro un banner de estado cerrado
-#   state = "closed"
+    state = None
 
-if soup.find("span", attrs={"title": "Status: Closed"}):
-    state = "closed"
+    isOpen = soup.find("span", attrs={"title": "Status: Open"})
+    if isOpen:
+        ### ver si está el octicon octicon octicon-issue-opened
+        if isOpen.findChildren("svg", attrs={"class": "octicon octicon-issue-opened"}):
+            state = "open"
+
+    if soup.find(
+        "span", attrs={"title": "Status: Closed", "class": "State State--merged"}
+    ):
+        state = "closed"
+
+    if state is not None:
+        author = soup.find(
+            "a", attrs={"class": "author text-bold Link--secondary"}
+        ).get_text()
+
+        assignees = (
+            soup.find("span", attrs={"class": "css-truncate js-issue-assignees"})
+            .get_text()
+            .replace("\n", "")
+            .split()
+        )
+
+        if assignees == ["No", "one", "assigned"]:
+            assignees = ["No one assigned"]
+
+        for l in range(len(assignees)):
+            df = df.append(
+                {
+                    "issue": issue,
+                    "author": author,
+                    "state": state,
+                    "assignees": assignees[l],
+                },
+                ignore_index=True,
+            )
+    issue += 1
 
 
-author = soup.find("a", attrs={"class": "author text-bold Link--secondary"}).get_text()
-assignees = (
-    soup.find("span", attrs={"class": "css-truncate js-issue-assignees"})
-    .get_text()
-    .replace("\n", "")
-    .split()
-)
-
-if assignees == ["No", "one", "assigned"]:
-    assignees = ["No one assigned"]
-
-
-print(issue, author, state, assignees)
-
-for l in range(len(assignees)):
-    df = df.append(
-        {"issue": issue, "author": author, "state": state, "assignees": assignees[l]},
-        ignore_index=True,
-    )
+df.to_csv("issues.csv", index=False)
