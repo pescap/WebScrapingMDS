70a89600bc3cf3d6b396ef6c788b15ab2f3ffa86
jwilenmann <javier.wilenmann@uai.cl>
2022-08-30 20:35:41 -0400

    corrección error scraper y requirements

--- producto7/scraper.py
+++ producto7/scraper.py
@@ -8,6 +8,7 @@ import pandas as pd
 import time
 import os
 import sys
+import locale
 
 
 
@@ -37,7 +38,7 @@ driver.get(url)
 
 driver.implicitly_wait(10)
 
-anos = ["2015", "2016", "2017, "2018", "2019"]
+anos = ["2015", "2016", "2017", "2018", "2019"]
 meses = ["01", "02", "03", "04", "05", "06", "07", "08", '09', '10', '11', '12']
 
 
