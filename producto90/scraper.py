b0183026cf080858ea9b82a8bd5093399f909e59
molivadatascience <110955892+molivadatascience@users.noreply.github.com>
2022-10-01 23:42:15 -0400

    Update scraper.py

--- producto90/scraper.py
+++ producto90/scraper.py
@@ -15,10 +15,10 @@ import pandas as pd
 import time
 import os
 
-chrome_options = Opciones()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
+
+chromeOptions.add_argument('--headless')
+chromeOptions.add_argument('--no-sandbox')
+chromeOptions.add_argument('--disable-dev-shm-usage')
 
 # Se define la ruta
 chromeOptions = webdriver.ChromeOptions()
